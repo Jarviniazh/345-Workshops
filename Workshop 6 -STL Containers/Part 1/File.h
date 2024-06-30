a26193f7fbfe837d45b426bfbde19f849f53a691
Jarviniazh <zhaojwdkj@gmail.com>
2024-06-29 20:49:07 -0400

    Add provided code for this workshop

---
+++ Workshop 6 -STL Containers/Part 1/File.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Resource.h"
+
+namespace seneca
+{
+	class File : public Resource
+	{
+		//std::string m_filename{};
+		std::string m_contents{};
+	public:
+		//a custom constructor that receives two strings as parameters
+		File(const std::string& filename, const std::string & content = "");
+
+		//define the pure virtual functions inherited from the base class
+		//sets the parent path to the parameter
+		void update_parent_path(const std::string&);
+		//a query that returns the name of the file.
+		std::string name() const;
+		//a query that returns - 1.
+		int count() const;
+		//a query that returns the full absolute path of the file (concatenates the absolute path location and the file name).
+		std::string path() const;
+		//a query that returns the size of the file in bytes.For simplicity, this is the number of characters in m_contents attribute.
+		size_t size() const;
+		//a query that returns Flags::FILE.
+		NodeType type() const;
+	
+
+
+	};
+}
